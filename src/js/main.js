25690966f530b41773f8efd452f4c611d777eaa1
Francesco Costi <fr.costi@gmail.com>
2024-09-11 22:45:35 +0700

    Enjoy Coding

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
+// Import the SCSS file
+// Don't delete this line or Vite will not compile the SCSS file
 import '../scss/style.scss'
 
 
+// You can start coding your project here
 console.log('Welcome to Chuski templating starter kit!!');
